f56f34c398440133fa8eb55fa2899db42e3df861
Alaaaya <128330294+Alaaaya@users.noreply.github.com>
2025-03-15 13:33:22 +0100

    script.js erstellen

---
+++ script.js
@@ -0,0 +1,6 @@
+)// تغيير شفافية شريط التنقل عند التمرير
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    let opacity = Math.min(0.8, window.scrollY / 300);
+    navbar.style.background = `rgba(0, 0, 0, ${opacity})`;
+});
